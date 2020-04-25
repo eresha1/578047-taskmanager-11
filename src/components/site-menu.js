05f74d996f5bbc07c080ec1bc54a644808869971
eresha1 <eresha39@gmail.com>
2020-04-25 19:12:04 +0700

    module4-task1

--- src/components/site-menu.js
+++ src/components/site-menu.js
@@ -1,4 +1,6 @@
-export const createSiteMenuTemplate = () => {
+import {createElement} from "../utils.js";
+
+const createSiteMenuTemplate = () => {
   return (
     `<section class="control__btn-wrap">
      <input type="radio" name="control" id="control__new-task" class="control__input visually-hidden" />
@@ -10,3 +12,25 @@ export const createSiteMenuTemplate = () => {
    </section>`
   );
 };
+
+export default class SiteMenu {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createSiteMenuTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
